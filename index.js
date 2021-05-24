67ade9250a6568dd972fc636957348da0d0d2dcf
MarlonMoreno <marlonmoreno894@gmail.com>
2021-05-23 22:17:47 -0500

    commit tree 212325b45549c3c43e996afe0c9d9717d0e78c65

--- index.js
+++ index.js
@@ -1,5 +1,76 @@
 /* Your Code Here */
 
+let createEmployeeRecord = function(employee) {
+    return {
+        firstName: employee[0],
+        familyName: employee[1],
+        title: employee[2],
+        payPerHour: employee[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+
+let createEmployeeRecords = function(arrays) {
+    return arrays.map(function(employee){
+        return createEmployeeRecord(employee)
+    })   
+}
+
+let createTimeInEvent = function(dateStamp) {
+    let [date, hour] = dateStamp.split(" ")
+
+    this.timeInEvents.push({
+        type: "TimeIn",
+        hour: parseInt(hour, 10),
+        date
+    })
+    return this
+}
+
+let createTimeOutEvent = function(dateStamp) {
+    let [date, hour] = dateStamp.split(" ")
+
+    this.timeOutEvents.push({
+        type: "TimeOut",
+        hour: parseInt(hour, 10),
+        date
+    })
+    return this
+}
+
+let hoursWorkedOnDate = function(date) {
+    let inTime = this.timeInEvents.find(function(e) {
+        return e.date === date
+    })
+
+    let outTime = this.timeOutEvents.find(function(e) {
+        return e.date === date
+    })
+    
+    return (outTime.hour - inTime.hour) / 100
+}
+
+let wagesEarnedOnDate = function(date) {
+    let hourlyRate = this.payPerHour * (hoursWorkedOnDate.call(this, date))
+
+    return hourlyRate
+}
+
+let findEmployeeByFirstName = function(array, firstName) {
+    let employee = array.find(function(employee){
+        return employee.firstName === firstName
+    })
+
+    return employee
+}
+
+let calculatePayroll = function(array) {
+    return array.reduce(function(total, employee){
+        return total + allWagesFor.call(employee)
+    }, 0)
+}
+
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
